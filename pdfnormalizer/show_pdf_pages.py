4672218a8a61673c04a40b42c30cf0b48abcda0d
lucasew <lucas59356@gmail.com>
2022-02-22 16:52:44 -0300

    show_pdf_pages as a base class
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- pdfnormalizer/show_pdf_pages.py
+++ pdfnormalizer/show_pdf_pages.py
@@ -5,7 +5,7 @@ import PySimpleGUI as sg
 import numpy as np
 import cv2
 from screeninfo import get_monitors, Enumerator
-from pdfnormalizer.utils import array_to_data
+from pdfnormalizer.utils import array_to_data, GUIWithImageTransformer
 
 def main():
     parser = ArgumentParser()
@@ -18,61 +18,10 @@ def main():
     page = args.n - 1
 
     images = convert_from_path(args.i)
-    images_arr = list(map(np.array, images))
     qt_images = len(images)
 
     if page < 0 or page >= qt_images:
         raise Exception(f"Invalid page: {page + 1}")
 
-    layout = [
-        [
-            sg.Button("<"),
-            sg.Button("+"),
-            sg.Button("X"),
-            sg.Button("-"),
-            sg.Button(">"),
-            sg.Text(key = 'label')
-        ],
-        [
-            sg.Image(key = 'img'),
-            sg.Image(key = 'img2'),
-        ]
-    ]
-
-
-    window = sg.Window("Imagens", layout, resizable = True, finalize = True, element_justification = 'c')
-    window.bind('<Configure>', 'Configure')
-    window.write_event_value('init', None)
-    while True:
-        event, values = window.read()
-        if event == 'X' or event == sg.WIN_CLOSED:
-            break
-        if event == '+':
-            scale += 0.1
-        if event == '-':
-            scale -= 0.1
-        if event == '<':
-            if page - 1 >= 0:
-                page -= 1
-        if event == '>':
-            if page + 1 < qt_images:
-                page += 1
-        (ww, wh) = window.Size
-        print(ww, wh)
-
-        img = images_arr[page]
-        (h, w, *_) = img.shape
-        w_prop = ((ww / 2) - 20) / w
-        h_prop = (wh - 50) / h
-        norm_scale = scale * min(w_prop, h_prop)
-        print(scale, norm_scale)
-        w = int(w*norm_scale)
-        h = int(h*norm_scale)
-
-        img = cv2.resize(img, fx = norm_scale, fy = norm_scale, dsize = (w, h))
-        window['img'].update(array_to_data(img))
-        window['img'].set_size((w, h))
-        window['img2'].update(array_to_data(img))
-        window['img2'].set_size((w, h))
-        window['label'].update(value = f'Página {page + 1} de {qt_images}')
-    window.close()
+    gui = GUIWithImageTransformer(images, start_page = page, scale = scale)
+    gui.run()
--- pdfnormalizer/utils.py
+++ pdfnormalizer/utils.py
@@ -1,3 +1,7 @@
+import numpy as np
+import PySimpleGUI as sg
+import cv2
+
 def array_to_data(array):
     from PIL import Image
     from io import BytesIO
@@ -7,4 +11,76 @@ def array_to_data(array):
         data = output.getvalue()
     return data
 
+class GUIWithImageTransformer():
+    scale = 1.0
+    page = 0
+    layout = [
+        [
+            sg.Button("<", key = 'prev'),
+            sg.Button("+", key = 'more_zoom'),
+            sg.Button("X"),
+            sg.Button("-", key = 'less_zoom'),
+            sg.Button(">", key = 'next'),
+            sg.Text(key = 'label')
+        ],
+        [
+            sg.Image(key = 'img'),
+            sg.Image(key = 'img2'),
+        ]
+    ]
 
+    def __init__(self, images = [], start_page = 0, scale = 1.0):
+        import numpy as np
+        self.scale = scale
+        self.page = start_page
+        self.images = list(map(np.array, images))
+        self.window = sg.Window('window', self.layout, resizable = True, finalize = True, element_justification = 'c')
+        self.window.bind('<Configure>', 'Configure')
+        self.window.write_event_value('init', None)
+    def handle_X(self, value):
+        return True
+    def handle_None(self, value):
+        return True
+    def handle_more_zoom(self, value):
+        self.scale += 0.1
+    def handle_less_zoom(self, value):
+        self.scale -= 0.1
+    def handle_prev(self, value):
+        if self.page > 0:
+            self.page -= 1
+    def handle_next(self, value):
+        if self.page < len(self.images) - 1:
+            self.page += 1
+    def frame_transform(self, frame):
+        return frame
+    def run(self):
+        while True:
+            event, values = self.window.read()
+            try:
+                handler = self.__getattribute__(f'handle_{event}')
+                ret = handler(values)
+                if ret is not None:
+                    break
+            except AttributeError:
+                pass
+            (ww, wh) = self.window.Size
+            print(ww, wh)
+
+            img = self.images[self.page]
+            (h, w, *_) = img.shape
+            w_prop = ((ww / 2) - 20) / w
+            h_prop = (wh - 50) / h
+            norm_scale = self.scale * min(w_prop, h_prop)
+            print(self.scale, norm_scale)
+            w = int(w*norm_scale)
+            h = int(h*norm_scale)
+
+            transformed_img = self.frame_transform(img)
+            img = cv2.resize(img, fx = norm_scale, fy = norm_scale, dsize = (w, h))
+            transformed_img = cv2.resize(transformed_img, fx = norm_scale, fy = norm_scale, dsize = (w, h))
+            self.window['img'].update(array_to_data(img))
+            self.window['img'].set_size((w, h))
+            self.window['img2'].update(array_to_data(transformed_img))
+            self.window['img2'].set_size((w, h))
+            self.window['label'].update(value = f'Page {self.page + 1} de {len(self.images)}')
+        self.window.close()
